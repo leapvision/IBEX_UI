ae174b432d601082cebf997628738f3cbb1a5039
Rajesh Kumar R <rajesh@drongoai.com>
2023-01-20 11:25:36 +0530

    Added invoice numbers in dispatch

--- src/app/pages/packing/dispatch/dispatch.component.ts
+++ src/app/pages/packing/dispatch/dispatch.component.ts
@@ -14,12 +14,14 @@ export class DispatchComponent implements OnInit {
   hideme: boolean[] = [false,false,true];
   breadCrumbItems: Array<{}>;
   containerNumbers = [];
+  invoiceNumbers = [];
   constructor() {
   }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Packing' }, { label: 'Dispatch', active: true }];
-    this.containerNumbers = ['MRKU09876SH','MRKU098456SH']
+    this.containerNumbers = ['MRKU09876SH','MRKU098456SH'];
+    this.invoiceNumbers = ['IEPL/0148/22-23','IEPL/0150/22-23','IEPL/0151/22-23','IEPL/0152/22-23'];
   }
 
   changeValue() {
