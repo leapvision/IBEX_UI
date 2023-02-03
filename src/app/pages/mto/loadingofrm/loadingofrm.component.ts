e566ae33d2e6c1673a4ba64cb25c27b14aab85f4
Sangam <sangam.naik1010@gmail.com>
2023-02-03 13:41:33 +0530

    Changes made in UI of mwo

--- src/app/pages/mto/loadingofrm/loadingofrm.component.ts
+++ src/app/pages/mto/loadingofrm/loadingofrm.component.ts
@@ -1,22 +1,11 @@
-import { Component, OnInit, ViewChildren, QueryList } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { DecimalPipe } from "@angular/common";
-import { Observable } from "rxjs";
-import { NgbDateStruct } from "@ng-bootstrap/ng-bootstrap";
-
-import {
-  OrderSortableService,
-  SortEvent,
-} from "./loadingofrm-sortable.directive";
-
-import { OrderService } from "./loadingofrm.service";
-import { Orders } from "./loadingofrm.model";
-import { ordersData } from "./data";
 
 @Component({
   selector: "app-loadingofrm",
   templateUrl: "./loadingofrm.component.html",
   styleUrls: ["./loadingofrm.component.scss"],
-  providers: [OrderService, DecimalPipe],
+  providers: [DecimalPipe],
 })
 export class LoadingOfRmComponent implements OnInit {
   // breadcrumb items
@@ -24,17 +13,7 @@ export class LoadingOfRmComponent implements OnInit {
   productionOrderNumbers = [];
   vendors = [];
 
-  ordersData: Orders[];
-
-  orders$: Observable<Orders[]>;
-  total$: Observable<number>;
-  model: NgbDateStruct;
-  @ViewChildren(OrderSortableService) headers: QueryList<OrderSortableService>;
-
-  constructor(public service: OrderService) {
-    this.orders$ = service.orders$;
-    this.total$ = service.total$;
-  }
+  constructor() {}
 
   ngOnInit(): void {
     this.breadCrumbItems = [
@@ -42,24 +21,7 @@ export class LoadingOfRmComponent implements OnInit {
       { label: "Loading of Raw Material", active: true },
     ];
 
-    this.ordersData = ordersData;
     this.productionOrderNumbers = ["1", "2", "3", "4", "5"];
     this.vendors = ["CMR", "Steel Line India", "Sunland"];
-  }
-
-  /**
-   * Sort table data
-   * @param param0 sort the column
-   *
-   */
-  onSort({ column, direction }: SortEvent) {
-    // resetting other headers
-    this.headers.forEach((header) => {
-      if (header.sortable !== column) {
-        header.direction = "";
-      }
-    });
-    this.service.sortColumn = column;
-    this.service.sortDirection = direction;
   }
 }
