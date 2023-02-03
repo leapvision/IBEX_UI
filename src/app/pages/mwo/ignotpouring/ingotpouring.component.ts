c9c78e37419d64b391367402aa2e42cb339cf274
Sangam <sangam.naik1010@gmail.com>
2023-02-03 18:35:34 +0530

    Added resuable ingot pouring report table

--- src/app/pages/mwo/ignotpouring/ingotpouring.component.ts
+++ src/app/pages/mwo/ignotpouring/ingotpouring.component.ts
@@ -1,28 +1,62 @@
-import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
-import { DecimalPipe } from '@angular/common';
-
+import { Component, OnInit, ViewChildren, QueryList } from "@angular/core";
+import { DecimalPipe } from "@angular/common";
+import { IgnotPouringService } from "./ingotpouring.service";
+import { MwoInspectionService } from "../mwoinspection/mwoinspection.service";
+import { SlagRemovingService } from "../slagremoving/slagremoving.service";
+import { TransferToMwoService } from "../transfertomwo/transfertomwo.service";
 
 @Component({
-  selector: 'app-ingotpouring',
-  templateUrl: './ingotpouring.component.html',
-  styleUrls: ['./ingotpouring.component.scss'],
-  providers: [ DecimalPipe]
-
+  selector: "app-ingotpouring",
+  templateUrl: "./ingotpouring.component.html",
+  styleUrls: ["./ingotpouring.component.scss"],
+  providers: [DecimalPipe],
 })
 export class IngotPouringComponent implements OnInit {
-  
-  hideme: boolean[] = [false,false,true];
+  hideme: boolean[] = [false, false, true];
   breadCrumbItems: Array<{}>;
 
-  constructor() {
-    this.breadCrumbItems = [{ label: 'MWO' }, { label: 'Ignot Pouring', active: true }];
-  }
+  constructor(
+    private transfertomwoService: TransferToMwoService,
+    private slagremovingService: SlagRemovingService,
+    private mwoinspectionService: MwoInspectionService,
+    private ingotpouringService: IgnotPouringService
+  ) {}
+
+  transfertomwoHeadingArray =
+    this.transfertomwoService.getTransferToMwoReport().heading;
+  transfertomwoBodyArray =
+    this.transfertomwoService.getTransferToMwoReport().body;
+  slagremovingHeadingArray =
+    this.slagremovingService.getSlagRemovingReport().heading;
+  slagremovingBodyArray = this.slagremovingService.getSlagRemovingReport().body;
+  mwoinspectionHeadingArray =
+    this.mwoinspectionService.getMwoInspectionReport().heading;
+  mwoinspectionBodyArray =
+    this.mwoinspectionService.getMwoInspectionReport().body;
+  ingotpouringHeadingArray =
+    this.ingotpouringService.getIgnotPouringReport().heading;
+  ingotpouringBodyArray = this.ingotpouringService.getIgnotPouringReport().body;
+  parentReports: Array<{}> = [
+    {
+      name: "Slag Removal",
+      heading: this.slagremovingHeadingArray,
+      body: this.slagremovingBodyArray,
+    },
+    {
+      name: "Transfer to MWO",
+      heading: this.transfertomwoHeadingArray,
+      body: this.transfertomwoBodyArray,
+    },
+  ];
 
   ngOnInit(): void {
+    this.breadCrumbItems = [
+      { label: "MWO" },
+      { label: "Ignot Pouring", active: true },
+    ];
   }
 
   changeValue() {
     this.hideme[2] = !this.hideme[2];
   }
-
 }
---
+++ src/app/pages/mwo/ignotpouring/ingotpouring.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class IgnotPouringService {
+  headingArray = [
+    [
+      { heading: "Sl No", rowspan: "1" },
+      { heading: "Ingot Pouring Date", rowspan: "1" },
+      { heading: "Shift & Time", rowspan: "1" },
+      { heading: "Hold Number", rowspan: "1" },
+      { heading: "Ingot Pouring", colspan: "4" },
+      { heading: "Ingot Details", colspan: "2" },
+      { heading: "Final Heat Number", rowspan: "1" },
+    ],
+    [
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "Temp(°C)" },
+      { heading: "Image" },
+      { heading: "Time" },
+      { heading: "Quantity(KG)" },
+      { heading: "No of Ingots Poured" },
+      { heading: "Ingot Weight" },
+      { heading: "" },
+    ],
+  ];
+  bodyArray = [
+    [
+      { value: "22/12/2022" },
+      { value: "" },
+      { value: "MW-01-1001" },
+      { value: "700" },
+      { img: "assets/images/small/img-3.jpg" },
+      { value: "10:00" },
+      { value: "550" },
+      { value: "75" },
+      { value: "547" },
+      { value: "IBEX-MW-01-1001-221222" },
+      { showParentReport: false },
+    ],
+    [
+      { value: "20/12/2022" },
+      { value: "" },
+      { value: "MW-01-1002" },
+      { value: "700" },
+      { img: "assets/images/small/img-3.jpg" },
+      { value: "10:00" },
+      { value: "500" },
+      { value: "70" },
+      { value: "540" },
+      { value: "IBEX-MW-01-1002-221220" },
+      { showParentReport: false },
+    ],
+    [
+      { value: "10/12/2022" },
+      { value: "" },
+      { value: "MW-01-1003" },
+      { value: "700" },
+      { img: "assets/images/small/img-3.jpg" },
+      { value: "10:00" },
+      { value: "500" },
+      { value: "70" },
+      { value: "540" },
+      { value: "IBEX-MW-01-1002-221220" },
+      { showParentReport: false },
+    ],
+  ];
+
+  constructor() {}
+
+  getIgnotPouringReport() {
+    return { heading: this.headingArray, body: this.bodyArray };
+  }
+}
