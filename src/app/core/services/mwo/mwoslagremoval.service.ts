3848aaccfd10e9d2900970e0f29bca19fe5d5d98
Sangam <sangam.naik1010@gmail.com>
2023-03-10 01:14:45 +0530

    Intregrated the GET and POST calls of slag removal in mwo with backend

---
+++ src/app/core/services/mwo/mwoslagremoval.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+@Injectable({
+  providedIn: "root",
+})
+export class MWOSlagRemovalService {
+  headingArray = [
+    [
+      { heading: "Sl No", rowspan: "1" },
+      { heading: "Slag Removal Date", rowspan: "1" },
+      { heading: "Shift", rowspan: "1" },
+      { heading: "Hold Number", rowspan: "1" },
+      { heading: "Addition Details", colspan: "5" },
+      { heading: "Metal Tapping to MWO", colspan: "4" },
+    ],
+    [
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "Be (Grams)" },
+      { heading: "Pure Alu (KG)" },
+      { heading: "Zinc (KG)" },
+      { heading: "Al Mn (KG)" },
+      { heading: "Pure Mg (KG)" },
+      { heading: "Tapping Temp", colspan: "2" },
+      { heading: "Time" },
+      { heading: "Quantity(KG)" },
+    ],
+    [
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "" },
+      { heading: "Value(°C)" },
+      { heading: "Image" },
+      { heading: "" },
+      { heading: "" },
+    ],
+  ];
+
+  apiurl = "http://localhost:8000/MWO/mwoSlagRemoval/";
+
+  constructor(private http: HttpClient) {}
+
+  getAllSlagRemovalReport(pageSize, pageNumber, searchValue) {
+    return this.http.get(
+      `http://localhost:8000/MWO/mwoSlagRemoval/?pageSize=${pageSize}&pageNumber=${pageNumber}&searchValue=${searchValue}`
+    );
+  }
+
+  getAllReadyForSlagRemoval() {
+    let url = "http://localhost:8000/MWO/readyForSlagRemoval/";
+    return this.http.get(url);
+  }
+
+  addSlagRemoval(slagRemovalData) {
+    return this.http.post(this.apiurl, slagRemovalData);
+  }
+}
--- src/app/pages/mwo/slagremoving/slagremoval.component.ts
+++ src/app/pages/mwo/slagremoving/slagremoval.component.ts
@@ -1,7 +1,18 @@
-import { Component, OnInit, ViewChildren, QueryList } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ViewChildren,
+  QueryList,
+  ViewChild,
+} from "@angular/core";
 import { DecimalPipe } from "@angular/common";
 import { TransferToMwoService } from "../transfertomwo/transfertomwo.service";
 import { SlagRemovingService } from "./slagremoving.service";
+import { WizardComponent } from "angular-archwizard";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MWOSlagRemovalService } from "src/app/core/services/mwo/mwoslagremoval.service";
+import { WebcamImage } from "ngx-webcam";
+import { dataURItoBlob } from "src/app/core/helpers/functions";
 
 @Component({
   selector: "app-slagremoval",
@@ -10,11 +21,30 @@ import { SlagRemovingService } from "./slagremoving.service";
   providers: [DecimalPipe],
 })
 export class SlagRemovalComponent implements OnInit {
+  @ViewChild(WizardComponent)
+  public wizard: WizardComponent;
+  webcamImage: WebcamImage | undefined;
   breadCrumbItems: Array<{}>;
 
+  pageSize: number = 10;
+  pageNumber: number = 1;
+  searchValue: string = "";
+  isRemarksAvailable: boolean = false;
+  isTappingTempImageAvailable: boolean = false;
+  imageFile: any;
+
+  holdNumbers = [];
+
+  mwoSlagRemovalForm: FormGroup;
+
+  bodyArray = [];
+  paginationDetails = {};
+
   constructor(
+    public fb: FormBuilder,
     private transfertomwoService: TransferToMwoService,
-    private slagremovingService: SlagRemovingService
+    private slagremovingService: SlagRemovingService,
+    private mwoSlagRemovalService: MWOSlagRemovalService
   ) {}
 
   transfertomwoHeadingArray =
@@ -23,22 +53,231 @@ export class SlagRemovalComponent implements OnInit {
   transfertomwoBodyArray =
     this.transfertomwoService.getTransferToMwoReportForMeltNumber("meltNumber")
       .body;
-  slagremovingHeadingArray =
-    this.slagremovingService.getSlagRemovingReport().heading;
-  slagremovingBodyArray = this.slagremovingService.getSlagRemovingReport().body;
-  parentReports: Array<{}> = [
-    {
-      name: "Transfer to MWO",
-      heading: this.transfertomwoHeadingArray,
-      body: this.transfertomwoBodyArray,
-      children: true,
-    },
-  ];
+  slagremovingHeadingArray = this.mwoSlagRemovalService.headingArray;
+  slagremovingBodyArray = [];
+  paginationData = {};
+
+  parentsReports = [];
 
   ngOnInit(): void {
     this.breadCrumbItems = [
       { label: "MWO" },
       { label: "Slag Removing", active: true },
     ];
+
+    this.formInit();
+
+    this.fetchHoldNumbers();
+
+    this.fetchMWOSlagRemovalReport(
+      this.pageSize,
+      this.pageNumber,
+      this.searchValue
+    );
+  }
+
+  formInit() {
+    this.mwoSlagRemovalForm = this.fb.group({
+      slagRemovalHoldNumberGroup: this.fb.group({
+        hold_no: [null, Validators.required],
+      }),
+      metalTappingToMWOGroup: this.fb.group({
+        tapping_temp: [null, Validators.required],
+        time: [null, Validators.required],
+        slag_quantity: [null, Validators.required],
+        slag_removal_quantity: [null, Validators.required],
+      }),
+    });
+  }
+
+  remarks(value) {
+    if (value && value.length > 0) {
+      this.isRemarksAvailable = true;
+    }
+  }
+
+  fetchHoldNumbers() {
+    let response;
+    this.mwoSlagRemovalService
+      .getAllReadyForSlagRemoval()
+      .subscribe((result) => {
+        if (result != null) {
+          response = result;
+          console.log(response);
+        }
+        this.holdNumbers = response.Data;
+      });
+  }
+
+  get form() {
+    return this.mwoSlagRemovalForm.controls;
+  }
+
+  onMwoSlagRemovalFormSubmit() {
+    let response;
+
+    const imageName = `${
+      this.form.slagRemovalHoldNumberGroup.value["hold_no"]
+    }-${new Date().getTime()}.png`;
+    const imageBlob = dataURItoBlob(this.webcamImage.imageAsBase64);
+    this.imageFile = new File([imageBlob], imageName, { type: "image/png" });
+    console.log(this.imageFile);
+
+    const slagRemovalFormData = new FormData();
+    slagRemovalFormData.append(
+      "holding_id",
+      this.holdNumbers.find(
+        (item) =>
+          item.hold_no === this.form.slagRemovalHoldNumberGroup.value["hold_no"]
+      ).id
+    );
+    slagRemovalFormData.append(
+      "line_id",
+      JSON.parse(localStorage.getItem("lineDetails")).id
+    );
+    slagRemovalFormData.append("addition_id", "1");
+    slagRemovalFormData.append(
+      "slag_quantity",
+      this.form.metalTappingToMWOGroup.value["slag_quantity"]
+    );
+    slagRemovalFormData.append(
+      "slag_removal",
+      this.form.metalTappingToMWOGroup.value["slag_removal_quantity"]
+    );
+    slagRemovalFormData.append(
+      "tapping_temp",
+      this.form.metalTappingToMWOGroup.value["tapping_temp"]
+    );
+    slagRemovalFormData.append("image_path", this.imageFile);
+    slagRemovalFormData.append(
+      "shift_id",
+      JSON.parse(localStorage.getItem("shiftDetails")).id
+    );
+
+    this.mwoSlagRemovalService
+      .addSlagRemoval(slagRemovalFormData)
+      .subscribe((result) => {
+        if (result != null) {
+          response = result;
+        }
+        console.log(response);
+        if (response.Result === "Success") {
+          this.mwoSlagRemovalForm.reset();
+          this.formInit();
+          this.wizard.reset();
+          this.fetchHoldNumbers();
+          this.fetchMWOSlagRemovalReport(
+            this.pageSize,
+            this.pageNumber,
+            this.searchValue
+          );
+        } else {
+          alert("Something went wrong!🥲");
+        }
+      });
+  }
+
+  fetchMWOSlagRemovalReport(pageSize, pageNumber, searchValue) {
+    let response;
+    this.mwoSlagRemovalService
+      .getAllSlagRemovalReport(pageSize, pageNumber, searchValue)
+      .subscribe((result) => {
+        if (result != null) {
+          response = result;
+        }
+        this.paginationDetails = {};
+        this.paginationDetails = response.Data.pagination;
+        this.paginationData = this.paginationDetails;
+        // console.log(response.Data.pagination);
+        // console.log(response.Data.records);
+        this.bodyArray = [];
+        this.parentsReports = [];
+        response.Data.records.forEach((item) => {
+          this.parentsReports = [
+            {
+              name: "Transfer To MWO",
+              heading: this.transfertomwoHeadingArray,
+              body: [
+                {
+                  currentReport: [
+                    {
+                      value: new Date(
+                        item["holding_details"]["created_on"]
+                      ).toLocaleDateString("en-GB"),
+                    },
+                    {
+                      value: item["holding_details"]["shift_details"]["name"],
+                    },
+                    {
+                      value: item["holding_details"]["melt_no"],
+                    },
+                    {
+                      value: item["holding_details"]["hold_no"],
+                    },
+                  ],
+                },
+              ],
+              children: true,
+            },
+          ];
+          this.bodyArray.push({
+            currentReport: [
+              {
+                value: new Date(item["created_on"]).toLocaleDateString("en-GB"),
+              },
+              { value: item["shift_details"]["name"] },
+              { value: item["holding_details"]["hold_no"] },
+              { value: 0 },
+              { value: 0 },
+              { value: 0 },
+              { value: 0 },
+              { value: 0 },
+              { value: item["tapping_temp"] },
+              {
+                img: `http://localhost:8000${item["image_path"]}`,
+              },
+              { value: item["slag_removal_time"] },
+              { value: item["slag_quantity"] },
+            ],
+            allPreviousReports: this.parentsReports,
+          });
+        });
+        this.slagremovingBodyArray = this.bodyArray;
+      });
+  }
+
+  onChangePageSize(pageSizeSelected) {
+    this.pageSize = pageSizeSelected;
+    if (pageSizeSelected < this.bodyArray.length) {
+      this.fetchMWOSlagRemovalReport(
+        this.pageSize,
+        this.pageNumber,
+        this.searchValue
+      );
+    }
+  }
+
+  onChangePageNumber(page) {
+    this.pageNumber = page;
+    this.fetchMWOSlagRemovalReport(
+      this.pageSize,
+      this.pageNumber,
+      this.searchValue
+    );
+  }
+
+  onChangeSearchValue(searchTerm) {
+    this.searchValue = searchTerm;
+    this.fetchMWOSlagRemovalReport(
+      this.pageSize,
+      this.pageNumber,
+      this.searchValue
+    );
+  }
+
+  handleImage(webcamImage: WebcamImage) {
+    this.webcamImage = webcamImage;
+    console.log(webcamImage);
+    this.isTappingTempImageAvailable = true;
   }
 }
